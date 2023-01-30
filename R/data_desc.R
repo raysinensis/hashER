5733458fd35d1c4837349cb36cbd71abf55751c4
raysinensis <raysinensis@gmail.com>
2023-01-30 15:53:42 -0500

    qc.R

--- R/data_desc.R
+++ R/data_desc.R
@@ -119,3 +119,12 @@ hash_data <- hash_data %>% rbind(
     code = ""
   )
 )
+
+hash_data <- hash_data %>% rbind(
+  data.frame(
+    sample_id = "Gau",
+    id_col = "sex_dmg",
+    ids = "F_M",
+    code = "str_sub(id_hash, 1, 1)"
+  )
+)
---
+++ R/qc.R
@@ -0,0 +1,76 @@
+test_bimodal <- function(mat,
+                         threshold = 10,
+                         return = "summary") {
+  mat <- mat[rowSums(mat) >= threshold, ]
+  if (nrow(mat) > 20000) {
+    mat <- mat[sample(1:nrow(mat), 20000), ]
+  }
+  res_lmoc <- apply(
+    mat,
+    MARGIN = 2, 
+    FUN = function(x) {
+      multimode::modetest(x)
+    }
+  )
+  
+  if (return == "all") {
+    return(res_lmoc)
+  } else if (return == "summary") {
+    res_lmoc2 <- map(res_lmoc, function(x) {
+      x$p.value
+    }) %>% unlist()
+    return(mean(res_lmoc2))
+  }
+}
+
+test_background <- function(mat,
+                            threshold = 10,
+                            q = 0.1,
+                            return = "summary") {
+  mat <- mat[rowSums(mat) >= threshold, ]
+  res_lmoc <- apply(
+    mat,
+    MARGIN = 2, 
+    FUN = function(x) {
+      multimode::locmodes(x)
+    }
+  )
+  
+  if (return == "all") {
+    return(res_lmoc)
+  } else if (return == "summary") {
+    res_lmoc2 <- map(res_lmoc, function(x) {
+      x$locations
+    }) %>% unlist()
+    return(max(res_lmoc2) / min(res_lmoc2) >= 10)
+  }
+}
+
+test_typebias <- function(mat,
+                          types,
+                          threshold = 10,
+                          return = "summary") {
+  mat <- mat[rowSums(mat) >= threshold, ]
+  cells <- split(Cells(so), so$type)
+  res <- map(cells, function(x) {
+    temp <- mat[x,]
+    apply(
+      temp,
+      MARGIN = 2, 
+      FUN = function(x) {
+        multimode::locmodes(x, mod0 = 2)
+      }
+    )
+  })
+
+  if (return == "all") {
+    return(res)
+  } else if (return == "summary") {
+    res2 <- map(res, function(y) {
+      map(y, function(x) {
+        x$locations[2]
+      })
+    } %>% unlist()) %>% as.data.frame() %>% t()
+    return(sum(matrixStats::colMaxs(res2) / matrixStats::colMins(res2) >=5) / ncol(res2))
+  }
+}
